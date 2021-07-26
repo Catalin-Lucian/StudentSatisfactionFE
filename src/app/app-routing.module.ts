50bb2b3511c524ff53abe7d787b9af7a3b43bb98
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2021-07-26 21:23:47 +0300

    suceesful log in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,110 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './components/page/page.component';
 
-const routes: Routes = [];
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbRegisterComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+  NbPasswordAuthStrategy,
+  NbAuthModule,
+} from '@nebular/auth';
+
+const routes: Routes = [
+  {
+    path:'page',
+    component:PageComponent,
+  },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        redirectTo:'login',
+        pathMatch:'full'
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+];
+
+//icoms to be shown at the bottom of page
+export interface NbAuthSocialLink {
+  link?: string,
+  url?: string,
+  target?: string,
+  title?: string,
+  icon?: string,
+}
+
+const socialLinks: NbAuthSocialLink[] = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+
+    RouterModule.forRoot(routes),
+    NbAuthModule.forRoot({
+      strategies: [
+        NbPasswordAuthStrategy.setup({
+          name: 'email',
+
+          baseEndpoint:'',
+          login: {
+            endpoint: '/api/Authenticate/login',
+            method: 'post',
+            requireValidToken: false,
+            redirect: {
+              success: '/page',
+              failure: null,
+            },
+            defaultErrors: ['Login/Email combination is not correct, please try again.'],
+            defaultMessages: ['You have been successfully logged in.'],
+          },
+          register:{
+            endpoint:'/api/Authenticate/register',
+            method:'post',
+          }
+        }),
+      ],
+      forms: {
+        login: {
+          redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
+          strategy: 'email',  // strategy id key.
+          rememberMe: false,   // whether to show or not the `rememberMe` checkbox
+          showMessages: {     // show/not show success/error messages
+            success: true,
+            error: true,
+          },
+          socialLinks: socialLinks, // social links at the bottom of a page
+        },
+      },
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbButtonModule,
+  NbChatModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbToastrModule,
+  NbWindowModule,
+}
+from '@nebular/theme';
+
+
+
+import { PageComponent } from './components/page/page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+
+    NbButtonModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbToastrModule.forRoot(),
+    NbChatModule.forRoot({
+      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
